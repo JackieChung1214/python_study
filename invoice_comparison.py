74f9ccf17f08119cc3eb26f7297a731eab7538ab
Jackie Chung <jackiechung1214@gmail.com>
2022-07-23 21:07:24 +0800

    加上計數功能

--- invoice_comparison.py
+++ invoice_comparison.py
@@ -1,7 +1,12 @@
+cnt=0
 mine=open('我的發票.txt').read().split()
 win=open('中獎號碼.txt').read().split()
 print(win)
 
 for i, num in enumerate(mine):
     if num in win:
+        cnt+=1
         print("第", i+1, "張發票對中號碼", num, "!")
+
+print("共", cnt, "張發票對中號碼 !!")
+
