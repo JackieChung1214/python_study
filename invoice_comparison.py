6d363df7d1af1eea9c98bc251246b28bfe18684e
Jackie Chung <jackiechung1214@gmail.com>
2022-07-23 21:01:46 +0800

    1. 發票對獎_1

---
+++ invoice_comparison.py
@@ -0,0 +1,7 @@
+mine=open('我的發票.txt').read().split()
+win=open('中獎號碼.txt').read().split()
+print(win)
+
+for i, num in enumerate(mine):
+    if num in win:
+        print("第", i+1, "張發票對中號碼", num, "!")
